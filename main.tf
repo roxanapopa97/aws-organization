5f8bd48206de35d30157834c8a4aa5d2b29c9769
roxanapopa97 <86410787+roxanapopa97@users.noreply.github.com>
2021-06-24 15:51:40 +0300

    Testing cross account assume role

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  alias = "test"
+  assume_role {
+    role_arn     = "arn:aws:iam::753739125648:role/TerraformAutomationRole"
+    session_name = "terraform-cicd"
+  }
+}
+
+resource "aws_s3_bucket" "b" {
+  provider = aws.test 
+  bucket   = "my-tf-test-bucket-570433319396"
+  tags     = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
